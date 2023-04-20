be87491af96eeba2a94cd15efa11a153358d4ecd
Hanboreum <matoma1_@naver.com>
2023-04-20 21:36:25 +0900

    Tcommit

--- midterm.R
+++ midterm.R
@@ -11,8 +11,17 @@ install.packages('wordcloud')
 library(wordcloud)
 #9
 wordcloud(words = [단어: 지역명],freq = [빈도수:Assault])
+#10
+foobar <-function()
+  
+  #11
+rm(list = ls())
 
-
+정상동작하지 않음
+install.packages('') #패키지 설치
+library()# 패키지 불러오기
+foobar <-data.frame(foo,bar)  # foobar 데이터 프레임 생성 
+foobar <-function() # 사용자 정의 함수 생성 
 #아래 오류가 발생해 실행하지 못했습니다
 install.packages('USArrests')
 WARNING: Rtools is required to build R packages but is not currently installed. Please download and install the appropriate version of Rtools before proceeding:
