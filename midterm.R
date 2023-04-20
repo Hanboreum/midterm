d767226898610fdd51ac29f1bce03df4e3daccc8
Hanboreum <matoma1_@naver.com>
2023-04-20 21:29:55 +0900

    Commit

--- midterm.R
+++ midterm.R
@@ -1,2 +1,14 @@
 install.packages('USArrests')
 library(USArrests)
+
+#아래 오류가 발생해 실행하지 못했습니다
+install.packages('USArrests')
+WARNING: Rtools is required to build R packages but is not currently installed. Please download and install the appropriate version of Rtools before proceeding:
+
+https://cran.rstudio.com/bin/windows/Rtools/
+Warning in install.packages :
+  package ‘USArrests’ is not available for this version of R
+
+A version of this package for your version of R might be available elsewhere,
+see the ideas at
+https://cran.r-project.org/doc/manuals/r-patched/R-admin.html#Installing-packages
